b3ee7f965525db70bf9901aac34c9071f1d84963
Steve Taylor <malakesh@hotmail.com>
2021-03-23 09:56:17 -0700

    Updates for App.js

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,20 @@
 import React from "react"
 import { hot } from "react-hot-loader/root"
-
 import { Route, Switch, BrowserRouter } from "react-router-dom"
-
+import TypeList from "./TypeList"
+// import AnimalList from "./AnimalList"
+// import SurrenderForm from "./SurrenderForm"
+import "../public/stlye.css"
 const App = props => {
-  return <div>Replace this div with your Router.</div>
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/pets" component={TypeList} />
+        {/* <Route exact path="/pets/new" component={SurrenderForm} /> */}
+        <Route exact path="/pets/:type" component={AnimalList} />
+        {/* <Route exact path="/pets/:type/:id" component={AnimalShow} /> */}
+      </Switch>
+    </BrowserRouter>
+  )
 }
-
 export default hot(App)
