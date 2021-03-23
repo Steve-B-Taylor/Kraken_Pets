0641168d2e778db95f2881735af06655fd7fc3e9
ikrisa10 <ilyakrisa@gmail.com>
2021-03-23 09:37:58 -0700

    added model, set up router, and corrected seeder

--- server/routes/rootRouter.js
+++ server/routes/rootRouter.js
@@ -1,10 +1,13 @@
 import express from "express"
 import clientRouter from "./clientRouter.js"
+import petsRouter from './api/v1/petsRouter.js'
 
 const rootRouter = new express.Router()
 
-rootRouter.use()
+rootRouter.use("/api/v1/pets", petsRouter)
 
 rootRouter.use("/", clientRouter)
+
+
 
 export default rootRouter
